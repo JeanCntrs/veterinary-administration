751c0fccffe190db424c0146ba5a80fa2671e442
Jean C. Contreras <jean.cntrs@gmail.com>
2020-01-12 20:57:14 -0300

    Updates

---
+++ precache-manifest.88405adb9c9d5d2fc71a6d12048263b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94d0ff2adea565146815be92dc409310",
+    "url": "/veterinary-administration/index.html"
+  },
+  {
+    "revision": "060202c7e9a14d1a2a98",
+    "url": "/veterinary-administration/static/js/2.4ef05891.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/veterinary-administration/static/js/2.4ef05891.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d5452e80be0e4ed81b3a",
+    "url": "/veterinary-administration/static/js/main.b0fd77d4.chunk.js"
+  },
+  {
+    "revision": "534711ff17dbe14cf12d",
+    "url": "/veterinary-administration/static/js/runtime-main.4d043a33.js"
+  }
+]);
